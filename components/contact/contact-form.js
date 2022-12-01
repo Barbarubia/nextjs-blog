0f538ca5c4739ef7ce915a42d0ac381ede9261ea
Barbarubia <dnl.desimone@gmail.com>
2022-12-01 16:59:46 +0100

    Pagina contatti collegata al database tramite api

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,28 +1,152 @@
+import Notification from "../ui/notification";
+import { useState, useEffect } from "react";
 import classes from "./contact-form.module.css";
 
+async function sendMessageData(messageDetails) {
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(messageDetails),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Si è verificato un problema!");
+  }
+}
+
 function ContactForm() {
+  // uso l'hook useState per settare i dati immessi nel form
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+  // uso l'hook useState per settare lo status del messaggio
+  const [requestStatus, setRequestStatus] = useState(); // "pending", "success", "error"
+  // uso l'hook useState per definire il tipo di errore
+  const [requestError, setRequestError] = useState();
+
+  // uso l'hook useEffect per cancellare le notifiche "success" e "error" dopo 3 secondi
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
+
+  async function sendMessage(event) {
+    // impedisco al browser di ricaricare la pagina quando effettuo il submit del form
+    event.preventDefault();
+
+    // TODO: effettuo la validazione client-side dei dati immessi nel form
+
+    // imposto lo status della request su "pending"
+    setRequestStatus("pending");
+
+    // effettuo il fetch dei dati immessi tramite il form all'API dei contatti
+    try {
+      await sendMessageData({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      });
+      // imposto lo status della request su "success"
+      setRequestStatus("success");
+      // Svuoto i campi del form
+      setEnteredEmail("");
+      setEnteredName("");
+      setEnteredMessage("");
+    } catch (error) {
+      // imposto lo status della request su "error"
+      setRequestError(error.message);
+      setRequestStatus("error");
+    }
+  }
+
+  // imposto gli stati della notifica
+  let notification;
+
+  if (requestStatus === "pending") {
+    notification = {
+      status: "pending",
+      title: "Attendi",
+      message: "Invio messaggio in corso!",
+    };
+  }
+
+  if (requestStatus === "success") {
+    notification = {
+      status: "success",
+      title: "Inviato!",
+      message: "Il tuo messaggio è stato inviato con successo!",
+    };
+  }
+
+  if (requestStatus === "error") {
+    notification = {
+      status: "error",
+      title: "Oh, no!",
+      message: requestError,
+    };
+  }
+
   return (
     <section className={classes.contact}>
       <h1>Contattami</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessage}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">La tua e-mail</label>
-            <input type="email" id="email" placeholder="esempio@email.com" required />
+            <input
+              type="email"
+              id="email"
+              placeholder="esempio@email.com"
+              required
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Il tuo nome</label>
-            <input type="text" id="name" placeholder="Mario Rossi" required />
+            <input
+              type="text"
+              id="name"
+              placeholder="Mario Rossi"
+              required
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Il tuo messaggio</label>
-          <textarea id="message" rows="5" placeholder="Scrivi qui il tuo messaggio" required></textarea>
+          <textarea
+            id="message"
+            rows="5"
+            placeholder="Scrivi qui il tuo messaggio"
+            required
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
-            <button>Invia il messaggio</button>
+          <button>Invia il messaggio</button>
         </div>
       </form>
+      {/* Se la variabile notification non è undefined visualizzo il componente Notification */}
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 }
---
+++ pages/api/contact.js
@@ -0,0 +1,68 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  // Per la pagina dei contatti l'unica request a cui sono interessato è il metodo POST
+  if (req.method === "POST") {
+    // estraggo i dati dal body della request
+    const { email, name, message } = req.body;
+
+    // effettuo la validazione server-side dei dati
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      // i dati non sono validi quindi restituisco una response di errore
+      res.status(422).json({ message: "Dati non validi!" });
+      return;
+    }
+
+    // se i dati sono ok vado avanti connettendomi al database MongoDB
+    let client;
+
+    try {
+      // TODO: sostituire "<NOME>" e "<PASSWORD>" con le credenziali del proprio database
+      client = await MongoClient.connect(
+        "mongodb+srv://<NOME>:<PASSWORD>@cluster0.gcpvcae.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      // Errore se non riesco a connettermi al database
+      res.status(500).json({ message: "Impossibile connettersi al database." });
+      return;
+    }
+
+    // se la connessione al database avviene con successo proseguo
+    const db = client.db("nextjs-blog"); // imposto il nome del database (nextjs-blog in questo caso)
+
+    // creo nel database una collection con il nome di "messages" e inserisco all'interno della stessa i dati del messaggio (salvati in un oggetto "newMessage")
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      // imposto l'id del messaggio con quello generato automaticamente dal database
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      // se ho un errore:
+      // chiudo la connessione al database
+      client.close();
+      // imposto la response con un messaggio di errore
+      res.status(500).json({ message: "Salvataggio messaggio fallito!" });
+      return;
+    }
+
+    // dopo aver salvato con successo il messaggio, chiudo la connessione al database
+    client.close();
+
+    // restituisco una response di successo
+    res.status(201).json({ message: "Messaggio inviato con successo!" });
+  }
+}
+
+export default handler;
