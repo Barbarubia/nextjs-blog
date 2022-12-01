4de07a079aade5151971b82256ab4d6c6696c68f
Barbarubia <dnl.desimone@gmail.com>
2022-12-01 12:49:55 +0100

    modifica titolo pagina

--- components/posts/all-posts.js
+++ components/posts/all-posts.js
@@ -4,7 +4,7 @@ import classes from "./all-posts.module.css";
 function AllPosts(props) {
   return (
     <section className={classes.posts}>
-      <h1>All Posts</h1>
+      <h1>Tutti gli articoli</h1>
       <PostsGrid posts={props.posts} />
     </section>
   );
